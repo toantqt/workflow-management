7926d3a53b8d5486be1c68ad4c0a7b4de3595706
toantqt <toantqt0609@gmail.com>
2020-07-07 17:32:47 +0700

    test server

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 6699;
+
+app.listen(port, () => {
+  console.log("server on port: " + port);
+});
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
