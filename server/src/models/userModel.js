61c2a0e42fce5207fb8a00d5e9e9af593a2655be
toantqt <toantqt0609@gmail.com>
2020-07-08 11:44:31 +0700

    add hash password

--- server/src/models/userModel.js
+++ server/src/models/userModel.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt");
+
 const userSchema = new Schema(
   {
     username: { type: String },
@@ -8,7 +10,7 @@ const userSchema = new Schema(
     profile: {
       avatar: { type: String },
       gender: { type: String },
-      address: { type: String,default:"" },
+      address: { type: String, default: "" },
     },
     role: { type: String },
   },
@@ -21,3 +23,11 @@ const userSchema = new Schema(
 );
 
 module.exports = mongoose.model("User", userSchema);
+module.exports.hashPassword = async (password) => {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    return await bcrypt.hash(password, salt);
+  } catch (error) {
+    throw new Error("Hashing failed");
+  }
+};
