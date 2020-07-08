015f61f433409da55b629f2d97a427a12b6e3e79
toantqt <toantqt0609@gmail.com>
2020-07-08 10:57:37 +0700

    add joi helper

---
+++ server/src/helpers/joi.helper.js
@@ -0,0 +1,9 @@
+const Joi = require("@hapi/joi");
+
+let userSchema = Joi.object().keys({
+  email: Joi.string().email().required(),
+  username: Joi.string().required(),
+  password: Joi.string().required(),
+});
+
+module.exports = userSchema;
