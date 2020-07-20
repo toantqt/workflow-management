1bdc4c604ca2fe43ee52b1e2d30fa77edccc6f64
huuphat98ss <huuphat98s@gmail.com>
2020-07-20 08:50:08 +0700

    tastmodel db

--- server/src/models/taskModel.js
+++ server/src/models/taskModel.js
@@ -1,21 +1,29 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 let Schema = mongoose.Schema;
-let taskSchema = new Schema({
-    title:{type: String},
-    list:[
-        {
-            name:String,
-            key: Boolean,default:false
-        }
+let taskSchema = new Schema(
+  {
+    roomId: String,
+    title: { type: String },
+    list: [
+      {
+        name: String,
+        key: Boolean,
+        default: false,
+      },
     ],
-    idStaff:{type: String},
-    note:{type: String}
-},{
-    timestamps:{
-            createdAt: 'createAt',
-            updatedAt: 'updateAt'
-    }
-});
+    idStaff: { type: String },
+    note: { type: String },
+    start: { type: Date, default: Date.now },
+    deadline: { type: Date, default: null },
+    done: { type: Boolean, default: false },
+  },
+  {
+    timestamps: {
+      createdAt: "createAt",
+      updatedAt: "updateAt",
+    },
+  }
+);
 
-module.exports = mongoose.model('Task',taskSchema);
+module.exports = mongoose.model("Task", taskSchema);
